1852e4abc6332b032164776a8c048d7afdb74876
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-02-14 16:49:48 +0100

    Minor cleanup

--- wc-orders-search-algolia.php
+++ wc-orders-search-algolia.php
@@ -12,41 +12,38 @@ define('AOS_VERSION', '0.1.0');
 add_action(
     'plugins_loaded',
     function () {
-        // Check `composer install` has been ran
-        if (file_exists(dirname(__FILE__).'/vendor')) {
-            // Composer dependencies
-            require_once 'vendor/autoload.php';
+        // Composer dependencies
+        require_once 'vendor/autoload.php';
 
-            // Resources
-            require_once 'inc/OrderChangeListener.php';
-            require_once 'inc/OrdersIndex.php';
-            require_once 'inc/Options.php';
-            require_once 'inc/Plugin.php';
+        // Resources
+        require_once 'inc/OrderChangeListener.php';
+        require_once 'inc/OrdersIndex.php';
+        require_once 'inc/Options.php';
+        require_once 'inc/Plugin.php';
 
-            $plugin = \AlgoliaOrdersSearch\Plugin::initialize(new \AlgoliaOrdersSearch\Options());
-            // $plugin->getOptions()->clearAlgoliaAccountSettings();
+        $plugin = \AlgoliaOrdersSearch\Plugin::initialize(new \AlgoliaOrdersSearch\Options());
 
-            if (is_admin()) {
-                require_once 'inc/admin/OptionsPage.php';
-                require_once 'inc/admin/OrdersListPage.php';
-                require_once 'inc/admin/AjaxReindex.php';
-                require_once 'inc/admin/AjaxIndexingOptionsForm.php';
-                require_once 'inc/admin/AjaxAlgoliaAccountSettingsForm.php';
-                new \AlgoliaOrdersSearch\Admin\OptionsPage($plugin->getOptions());
-                new \AlgoliaOrdersSearch\Admin\OrdersListPage($plugin->getOptions());
-                if ($plugin->getOptions()->hasAlgoliaAccountSettings()) {
-                    new \AlgoliaOrdersSearch\Admin\AjaxReindex($plugin->getOrdersIndex(), $plugin->getOptions());
-                }
-                new \AlgoliaOrdersSearch\Admin\AjaxIndexingOptionsForm($plugin);
-                new \AlgoliaOrdersSearch\Admin\AjaxAlgoliaAccountSettingsForm($plugin->getOptions());
+        if (is_admin()) {
+            require_once 'inc/admin/OptionsPage.php';
+            require_once 'inc/admin/OrdersListPage.php';
+            require_once 'inc/admin/AjaxReindex.php';
+            require_once 'inc/admin/AjaxIndexingOptionsForm.php';
+            require_once 'inc/admin/AjaxAlgoliaAccountSettingsForm.php';
+            new \AlgoliaOrdersSearch\Admin\OptionsPage($plugin->getOptions());
+            new \AlgoliaOrdersSearch\Admin\OrdersListPage($plugin->getOptions());
+            if ($plugin->getOptions()->hasAlgoliaAccountSettings()) {
+                new \AlgoliaOrdersSearch\Admin\AjaxReindex($plugin->getOrdersIndex(), $plugin->getOptions());
             }
-
-            // WP CLI commands
-            if (defined('WP_CLI') && WP_CLI) {
-                require_once 'inc/Commands.php';
-                $commands = new \AlgoliaOrdersSearch\Commands($plugin->getOrdersIndex(), $plugin->getOptions());
-                WP_CLI::add_command('orders', $commands);
-            }
+            new \AlgoliaOrdersSearch\Admin\AjaxIndexingOptionsForm($plugin);
+            new \AlgoliaOrdersSearch\Admin\AjaxAlgoliaAccountSettingsForm($plugin->getOptions());
         }
+
+        // WP CLI commands
+        if (defined('WP_CLI') && WP_CLI) {
+            require_once 'inc/Commands.php';
+            $commands = new \AlgoliaOrdersSearch\Commands($plugin->getOrdersIndex(), $plugin->getOptions());
+            WP_CLI::add_command('orders', $commands);
+        }
+
     }
 );
