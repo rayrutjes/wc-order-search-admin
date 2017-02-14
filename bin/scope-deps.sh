b520befa5fc9ea6dda7703cb49f2f3dc758e711c
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-02-14 12:02:25 +0100

    Add comment

--- bin/scope-deps.sh
+++ bin/scope-deps.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Todo: abstract this to make it more re-usable.
+
 find ./vendor -name "*.php" -exec sed -i 's/AlgoliaSearch/AlgoliaOrdersSearch/g' {} \;
 find ./vendor -name "composer.json" -exec sed -i 's/AlgoliaSearch/AlgoliaOrdersSearch/g' {} \;
 mv ./vendor/algolia/algoliasearch-client-php/src/AlgoliaSearch ./vendor/algolia/algoliasearch-client-php/src/AlgoliaOrdersSearch 2>/dev/null
