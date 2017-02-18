335a831faf23eb882c27e6747d2ee8dd01e49652
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-02-18 18:02:05 +0100

    Update build script

--- bin/build-archive.sh
+++ bin/build-archive.sh
@@ -7,7 +7,7 @@ mkdir ./build/wc-orders-search-algolia
 
 cp -R ./assets ./build/wc-orders-search-algolia
 cp -R ./inc ./build/wc-orders-search-algolia
-cp -R ./vendor ./build/wc-orders-search-algolia
+cp -R ./libs ./build/wc-orders-search-algolia
 cp wc-orders-search-algolia.php ./build/wc-orders-search-algolia
 
 cd ./build
