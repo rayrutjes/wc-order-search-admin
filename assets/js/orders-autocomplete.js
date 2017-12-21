27bba446208629f3bbae7109bd5662bd1abccfdf
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-12-21 17:36:47 +0100

    feat(autocomplete): display the best matching full name and email
    Closes: #38

--- assets/js/orders-autocomplete.js
+++ assets/js/orders-autocomplete.js
@@ -1,11 +1,11 @@
 var client = algoliasearch(aosOptions.appId, aosOptions.searchApiKey);
 var index = client.initIndex(aosOptions.ordersIndexName);
-autocomplete('#post-search-input', { hint: false, openOnFocus: true, debug: aosOptions.debug }, [
+autocomplete('#post-search-input', {hint: false, openOnFocus: true, debug: aosOptions.debug}, [
   {
-    source: autocomplete.sources.hits(index, { hitsPerPage: 7 }),
+    source: autocomplete.sources.hits(index, {hitsPerPage: 7}),
     displayKey: 'number',
     templates: {
-      suggestion: function(suggestion) {
+      suggestion: function (suggestion) {
         return ""
           + getStatusLine(suggestion)
           + getNumberLine(suggestion)
@@ -15,15 +15,15 @@ autocomplete('#post-search-input', { hint: false, openOnFocus: true, debug: aosO
       }
     }
   }
-]).on('autocomplete:selected', function(event, suggestion, dataset) {
-    window.location.href = "post.php?post=" + suggestion.objectID + "&action=edit";
+]).on('autocomplete:selected', function (event, suggestion) {
+  window.location.href = "post.php?post=" + suggestion.objectID + "&action=edit";
 });
 jQuery('#post-search-input').focus();
 
 function getStatusLine(suggestion) {
   return '<div class="wc-osa__line">'
-  + '<span class="wc-osa__status">' + suggestion._highlightResult.status_name.value + '</span>'
-  + '</div>';
+    + '<span class="wc-osa__status">' + suggestion._highlightResult.status_name.value + '</span>'
+    + '</div>';
 }
 
 function getNumberLine(suggestion) {
@@ -34,12 +34,47 @@ function getNumberLine(suggestion) {
 }
 
 function getCustomerLine(suggestion) {
-  if(typeof suggestion.customer === 'undefined') {
+  if (typeof suggestion.customer === 'undefined') {
     return 'Anonymous user';
   }
 
-  return suggestion._highlightResult.customer.display_name.value
-    + ' (' + suggestion._highlightResult.customer.email.value + ')';
+  return getDisplayName(suggestion) + ' (' + getEmail(suggestion) + ')';
+}
+
+function getDisplayName(suggestion) {
+  if (typeof suggestion.customer !== 'undefined' && suggestion._highlightResult.customer.display_name.matchLevel !== 'none') {
+    return suggestion._highlightResult.customer.display_name.value;
+  }
+
+  if (typeof suggestion.billing !== 'undefined' && suggestion._highlightResult.billing.display_name.matchLevel !== 'none') {
+    return suggestion._highlightResult.billing.display_name.value;
+  }
+
+  if (typeof suggestion.shipping !== 'undefined' && suggestion._highlightResult.shipping.display_name.matchLevel !== 'none') {
+    return suggestion._highlightResult.shipping.display_name.value;
+  }
+
+  if (typeof suggestion.customer !== 'undefined') {
+    return suggestion._highlightResult.customer.display_name.value;
+  }
+
+  return 'Anonymous user';
+}
+
+function getEmail(suggestion) {
+  if (typeof suggestion.customer !== 'undefined' && suggestion._highlightResult.customer.email.matchLevel !== 'none') {
+    return suggestion._highlightResult.customer.email.value;
+  }
+
+  if (typeof suggestion.billing !== 'undefined' && suggestion._highlightResult.billing.email.matchLevel !== 'none') {
+    return suggestion._highlightResult.billing.email.value;
+  }
+
+  if (typeof suggestion.customer !== 'undefined') {
+    return suggestion._highlightResult.customer.email.value;
+  }
+
+  return 'Unknown email';
 }
 
 function getTotalsLine(suggestion) {
@@ -51,16 +86,16 @@ function getTotalsLine(suggestion) {
 
 function getMethodsLine(suggestion) {
   var html = '';
-  if(suggestion.shipping_method_title) {
+  if (suggestion.shipping_method_title) {
     html += '<span class="wc-osa__shipping-method">' + suggestion.shipping_method_title + '</span>';
   }
-  if(suggestion.payment_method_title) {
+  if (suggestion.payment_method_title) {
     html += '<span class="wc-osa__payment-method">' + suggestion.payment_method_title + '</span>';
   }
 
-  if(html.length === 0) {
+  if (html.length === 0) {
     return '';
   }
 
-  return '<div class="wc-osa__line">'+ html + '</div>';
+  return '<div class="wc-osa__line">' + html + '</div>';
 }
