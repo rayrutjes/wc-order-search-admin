94a7fe53c6430163d2deb80c5aa252df2a214a83
Raymond Rutjes <raymond.rutjes@gmail.com>
2018-08-09 16:24:06 +0200

    chore: use gsed instead of sed in scripts (#75)

--- bin/publish.sh
+++ bin/publish.sh
@@ -16,7 +16,7 @@ fi
 readonly PACKAGE_VERSION=$(< package.json grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
+  | gsed 's/[",]//g' \
   | tr -d '[:space:]')
 
 git tag "v$PACKAGE_VERSION"
