99cd6e236a767efaa23126bd350ff8efc66f0838
Henrik Urlund <henrik@urlund.com>
2018-12-05 17:36:23 +0100

    fix(validation): also adds validation to constants

--- includes/class-options.php
+++ includes/class-options.php
@@ -39,16 +39,26 @@ class Options {
 	}
 
 	public function set_algolia_account_settings( $app_id, $search_key, $admin_key ) {
-		$app_id = trim( $app_id );
+		$app_id = defined( 'WC_OSA_ALGOLIA_APPLICATION_ID' ) ? WC_OSA_ALGOLIA_APPLICATION_ID : trim( $app_id ) ;
 		$this->assert_not_empty( $app_id, 'Algolia application ID' );
-		$search_key = trim( $search_key );
+
+		$search_key = defined( 'WC_OSA_ALGOLIA_SEARCH_API_KEY' ) ? WC_OSA_ALGOLIA_SEARCH_API_KEY : trim( $search_key ) ;
 		$this->assert_not_empty( $search_key, 'Algolia search only API key' );
-		$admin_key = trim( $admin_key );
+
+		$admin_key = defined( 'WC_OSA_ALGOLIA_ADMIN_API_KEY' ) ? WC_OSA_ALGOLIA_ADMIN_API_KEY : trim( $admin_key ) ;
 		$this->assert_not_empty( $admin_key, 'Algolia admin API key' );
 
-		update_option( 'wc_osa_alg_app_id', $app_id );
-		update_option( 'wc_osa_alg_search_api_key', $search_key );
-		update_option( 'wc_osa_alg_admin_api_key', $admin_key );
+		if ( ! defined ( 'WC_OSA_ALGOLIA_APPLICATION_ID' ) ) {
+			update_option( 'wc_osa_alg_app_id', $app_id );
+		}
+
+		if ( ! defined ( 'WC_OSA_ALGOLIA_SEARCH_API_KEY' ) ) {
+			update_option( 'wc_osa_alg_search_api_key', $search_key );
+		}
+
+		if ( ! defined ( 'WC_OSA_ALGOLIA_ADMIN_API_KEY' )) {
+			update_option( 'wc_osa_alg_admin_api_key', $admin_key );
+		}
 	}
 
 	public function get_orders_index_name() {
@@ -56,9 +66,12 @@ class Options {
 	}
 
 	public function set_orders_index_name( $orders_index_name ) {
-		$orders_index_name = trim( (string) $orders_index_name );
+		$orders_index_name = defined ( 'WC_OSA_ORDERS_INDEX_NAME' ) ? WC_OSA_ORDERS_INDEX_NAME : trim( (string) $orders_index_name ) ;
 		$this->assert_not_empty( $orders_index_name, 'Orders index name' );
-		update_option( 'wc_osa_orders_index_name', $orders_index_name );
+
+		if ( ! defined ( 'WC_OSA_ORDERS_INDEX_NAME' ) ) {
+			update_option( 'wc_osa_orders_index_name', $orders_index_name );
+		}
 	}
 
 	/**
@@ -69,12 +82,18 @@ class Options {
 	}
 
 	public function set_orders_to_index_per_batch_count( $per_batch ) {
+		if ( defined( 'WC_OSA_ORDERS_PER_BATCH' ) && (int) WC_OSA_ORDERS_PER_BATCH <= 0 ) {
+			throw new \InvalidArgumentException( 'Orders to index per batch should be greater than 0.' );
+		}
+
 		$per_batch = (int) $per_batch;
 		if ( $per_batch <= 0 ) {
 			$per_batch = 500;
 		}
 
-		update_option( 'wc_osa_orders_per_batch', $per_batch );
+		if ( ! defined ( 'WC_OSA_ORDERS_INDEX_NAME' ) ) {
+			update_option( 'wc_osa_orders_per_batch', $per_batch );
+		}
 	}
 
 	private function assert_not_empty( $value, $attribute_name ) {
